15539bbf5f5d0a001b355ff993f4583b7d261261
BuianAlex <a.buyan@ukr.net>
2019-11-07 15:21:10 +0200

    fix input dialog

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,21 @@ module.exports = {
     browser: true,
     es6: true
   },
-  extends: ["airbnb-base"],
+  extends: ["airbnb", "prettier"],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly"
   },
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
     ecmaVersion: 2018,
     sourceType: "module"
   },
+  plugins: ["prettier"],
   rules: {
-    "linebreak-style": 0,
-    "global-require": 0,
-    "eslint linebreak-style": [0, "error", "windows"]
-  },
-  settings: {
-    "import/resolver": {
-      node: {}
-    }
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }]
   }
 };
--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-import { MDCDialog } from '@material/dialog';
+import { MDCDialog } from "@material/dialog";
 
 function toDate(time) {
-  let datastring = '';
+  let datastring = "";
   const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
   ];
   if (time) {
     const d = new Date(time);
@@ -27,63 +27,63 @@ function toDate(time) {
 
 const testTasks = [
   {
-    id: '1',
-    name: 'Learn HTML',
+    id: "1",
+    name: "Learn HTML",
     time: 1573082126942,
-    status: true,
+    status: true
   },
   {
-    id: '2',
-    name: 'Learn CSS',
+    id: "2",
+    name: "Learn CSS",
     time: 1573082126942,
-    status: true,
+    status: true
   },
   {
-    id: '3',
-    name: 'JavaScript',
+    id: "3",
+    name: "JavaScript",
     time: 1573082126942,
-    status: false,
+    status: false
   },
   {
-    id: '4',
-    name: 'Learn React',
+    id: "4",
+    name: "Learn React",
     time: 1573082126942,
-    status: false,
+    status: false
   },
   {
-    id: '5',
-    name: 'Learn Node.js',
+    id: "5",
+    name: "Learn Node.js",
     time: 1573082126942,
-    status: false,
+    status: false
   },
   {
-    id: '6',
-    name: 'Learn how to rule the World',
+    id: "6",
+    name: "Learn how to rule the World",
     time: 1573082126942,
-    status: false,
-  },
+    status: false
+  }
 ];
 
 class ToDo {
   constructor() {
-    this.savedTasks = JSON.parse(localStorage.getItem('myTasks')) || testTasks;
-    this.wrapNodeTaskList = document.querySelector('#task-list');
-    this.calcSelected = document.querySelector('#calc-selected');
+    this.savedTasks = JSON.parse(localStorage.getItem("myTasks")) || testTasks;
+    this.wrapNodeTaskList = document.querySelector("#task-list");
+    this.calcSelected = document.querySelector("#calc-selected");
     this.select = [];
     this.calcSelected.textContent = ` ${this.select.length}`;
     this.renderTasks();
   }
 
   renderTasks() {
-    localStorage.setItem('myTasks', JSON.stringify(this.savedTasks));
-    this.wrapNodeTaskList.innerHTML = '';
-    this.savedTasks.forEach((item) => {
-      const taskseparator = document.createElement('li');
-      taskseparator.setAttribute('role', 'separator');
-      taskseparator.classList.add('mdc-list-divider');
-      const oneTask = document.createElement('li');
-      oneTask.classList.add('mdc-list-item');
-      oneTask.setAttribute('data_id', item.id);
+    localStorage.setItem("myTasks", JSON.stringify(this.savedTasks));
+    this.wrapNodeTaskList.innerHTML = "";
+    this.savedTasks.forEach(item => {
+      const taskseparator = document.createElement("li");
+      taskseparator.setAttribute("role", "separator");
+      taskseparator.classList.add("mdc-list-divider");
+      const oneTask = document.createElement("li");
+      oneTask.classList.add("mdc-list-item");
+      oneTask.setAttribute("data_id", item.id);
       oneTask.innerHTML = `
       <div class="mdc-form-field">
         <div class="mdc-checkbox">
@@ -103,11 +103,11 @@ class ToDo {
       </div>
       </div>
       <span class="mdc-list-item__text" ${
-  item.status ? 'style="text-decoration:line-through"' : false
-}>${item.name}</span>
+        item.status ? 'style="text-decoration:line-through"' : false
+      }>${item.name}</span>
       <span class="mdc-list-item__text mdc-layout-grid--align-right">${toDate(
-    item.time,
-  )}</span>`;
+        item.time
+      )}</span>`;
       this.wrapNodeTaskList.appendChild(oneTask);
       this.wrapNodeTaskList.append(taskseparator);
     });
@@ -116,14 +116,14 @@ class ToDo {
   addTask(text) {
     this.savedTasks.push({
       id: (
-        Date.now().toString(36)
-        + Math.random()
+        Date.now().toString(36) +
+        Math.random()
           .toString(36)
           .substr(2, 5)
       ).toUpperCase(),
       name: text,
       time: Date.now(),
-      status: false,
+      status: false
     });
     this.renderTasks();
   }
@@ -134,14 +134,14 @@ class ToDo {
   }
 
   unSelect(id) {
-    const temp = this.select.filter((item) => item !== id);
+    const temp = this.select.filter(item => item !== id);
     this.select = temp;
     this.calcSelected.textContent = ` ${this.select.length}`;
   }
 
   deleteTask() {
-    this.select.forEach((itemID) => {
-      const temp = this.savedTasks.filter((item) => item.id !== itemID);
+    this.select.forEach(itemID => {
+      const temp = this.savedTasks.filter(item => item.id !== itemID);
       this.savedTasks = temp;
     });
     this.select = [];
@@ -150,13 +150,13 @@ class ToDo {
   }
 
   setDone() {
-    this.select.forEach((itemID) => {
-      this.savedTasks.map((item) => {
+    this.select.forEach(itemID => {
+      this.savedTasks.map(item => {
         if (item.id === itemID) {
-          const newState = !item.status;
-          return newState;
+          item.status = !item.status;
+          return;
         }
-        return item;
+        return;
       });
     });
     this.select = [];
@@ -166,23 +166,23 @@ class ToDo {
 }
 
 const newTodo = new ToDo();
-const dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
-const addButton = document.querySelector('#add-task');
+const dialog = new MDCDialog(document.querySelector(".mdc-dialog"));
+const addButton = document.querySelector("#add-task");
 
-document.addEventListener('change', (e) => {
-  if (e.target.type === 'checkbox') {
+document.addEventListener("change", e => {
+  if (e.target.type === "checkbox") {
     if (e.target.checked) {
-      newTodo.addSelect(e.target.closest('li').getAttribute('data_id'));
+      newTodo.addSelect(e.target.closest("li").getAttribute("data_id"));
     } else {
-      newTodo.unSelect(e.target.closest('li').getAttribute('data_id'));
+      newTodo.unSelect(e.target.closest("li").getAttribute("data_id"));
     }
   }
 });
 
-document.addEventListener('keydown', (e) => {
-  if (e.target.type === 'text') {
+document.addEventListener("keydown", e => {
+  if (e.target.type === "text") {
     const saveBtn = document.querySelector(
-      'button[data-mdc-dialog-action="save"]',
+      'button[data-mdc-dialog-action="save"]'
     );
     if (e.target.value.length >= 3) {
       saveBtn.disabled = false;
@@ -193,23 +193,23 @@ document.addEventListener('keydown', (e) => {
 });
 
 document
-  .querySelector('#delete-task')
-  .addEventListener('click', () => newTodo.deleteTask());
+  .querySelector("#delete-task")
+  .addEventListener("click", () => newTodo.deleteTask());
 
 document
-  .querySelector('#set-done')
-  .addEventListener('click', () => newTodo.setDone());
+  .querySelector("#set-done")
+  .addEventListener("click", () => newTodo.setDone());
 
-addButton.addEventListener('click', () => {
+addButton.addEventListener("click", () => {
   dialog.open();
 });
 
-dialog.listen('MDCDialog:opened', () => {});
+dialog.listen("MDCDialog:opened", () => {});
 
-dialog.listen('MDCDialog:closing', (e) => {
-  if (e.detail.action === 'save') {
-    const taskName = document.querySelector('input[name = "task-name"]');
+dialog.listen("MDCDialog:closing", e => {
+  const taskName = document.querySelector('input[name = "task-name"]');
+  if (e.detail.action === "save") {
     newTodo.addTask(taskName.value);
-    taskName.value = '';
   }
+  taskName.value = "";
 });
