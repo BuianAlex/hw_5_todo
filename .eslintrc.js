0e4b7260925663bd2a202101866606c6e7d152a6
BuianAlex <a.buyan@ukr.net>
2019-11-07 14:59:08 +0200

    first draft

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,25 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
+    es6: true
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ["airbnb-base"],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module"
   },
   rules: {
+    "linebreak-style": 0,
+    "global-require": 0,
+    "eslint linebreak-style": [0, "error", "windows"]
   },
+  settings: {
+    "import/resolver": {
+      node: {}
+    }
+  }
 };
--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-import { MDCRipple } from '@material/ripple/index';
-import { MDCTopAppBar } from '@material/top-app-bar';
-import { MDCList } from '@material/list';
-import { MDCCheckbox } from '@material/checkbox';
-import { MDCMenu } from '@material/menu';
-import { MDCFormField } from '@material/form-field';
 import { MDCDialog } from '@material/dialog';
 
-Number.prototype.toDate = function () {
+function toDate(time) {
   let datastring = '';
   const months = [
     'January',
@@ -22,96 +16,48 @@ Number.prototype.toDate = function () {
     'November',
     'December',
   ];
-  if (this) {
-    const d = new Date(this);
+  if (time) {
+    const d = new Date(time);
     datastring = `${d.getHours()}:${d.getMinutes()} ${d.getDate()} ${
       months[d.getMonth()]
     } ${d.getFullYear()}`;
   }
   return datastring;
-};
-
-document.addEventListener('change', (e) => {
-  if (e.target.checked) {
-    newTodo.addSelected(e.target.closest('li').getAttribute('data_id'));
-  } else {
-    newTodo.dellSelected(e.target.closest('li').getAttribute('data_id'));
-  }
-});
-document
-  .querySelector('#delete-task')
-  .addEventListener('click', () => newTodo.deleteTask());
-
-const topAppBarElement = document.querySelector('.mdc-top-app-bar');
-const topAppBar = new MDCTopAppBar(topAppBarElement);
-const dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
-
-// const list = new MDCList(document.querySelector('.mdc-list'));
-// const listItemRipples = list.listElements.map(
-//   (listItemEl) => new MDCRipple(listItemEl),
-// );
-// const fabRipple = new MDCRipple(document.querySelector('.mdc-fab'));
-// const checkbox = new MDCCheckbox(document.querySelector('.mdc-checkbox'));
-// const formField = new MDCFormField(document.querySelector('.mdc-form-field'));
-// formField.input = checkbox;
-// const iconButtonRipple = new MDCRipple(
-//   document.querySelector('.mdc-icon-button'),
-// );
-// iconButtonRipple.unbounded = true;
-
-// const menu = new MDCMenu(document.querySelector('.mdc-menu'));
-// menu.open = false;
-
-// const menuButton = document.querySelector('#show_menu');
-// menuButton.addEventListener('click', () => {
-//   menu.open = !menu.open;
-// });
-const addButton = document.querySelector('#add-task');
-addButton.addEventListener('click', () => {
-  dialog.open();
-});
-dialog.listen('MDCDialog:opened', () => {
-  // Assuming contentElement references a common parent element with the rest of the page's content
-  console.log(dialog);
-});
-
-dialog.listen('MDCDialog:closing', () => {
-  console.log();
-});
+}
 
 const testTasks = [
   {
-    id: 1,
+    id: '1',
     name: 'Learn HTML',
     time: 1573082126942,
     status: true,
   },
   {
-    id: 2,
+    id: '2',
     name: 'Learn CSS',
     time: 1573082126942,
     status: true,
   },
   {
-    id: 3,
+    id: '3',
     name: 'JavaScript',
     time: 1573082126942,
     status: false,
   },
   {
-    id: 4,
+    id: '4',
     name: 'Learn React',
     time: 1573082126942,
     status: false,
   },
   {
-    id: 5,
+    id: '5',
     name: 'Learn Node.js',
     time: 1573082126942,
     status: false,
   },
   {
-    id: 6,
+    id: '6',
     name: 'Learn how to rule the World',
     time: 1573082126942,
     status: false,
@@ -120,7 +66,7 @@ const testTasks = [
 
 class ToDo {
   constructor() {
-    this.savedTasks = localStorage.getItem('myTasks') || testTasks;
+    this.savedTasks = JSON.parse(localStorage.getItem('myTasks')) || testTasks;
     this.wrapNodeTaskList = document.querySelector('#task-list');
     this.calcSelected = document.querySelector('#calc-selected');
     this.select = [];
@@ -129,53 +75,66 @@ class ToDo {
   }
 
   renderTasks() {
+    localStorage.setItem('myTasks', JSON.stringify(this.savedTasks));
     this.wrapNodeTaskList.innerHTML = '';
     this.savedTasks.forEach((item) => {
       const taskseparator = document.createElement('li');
       taskseparator.setAttribute('role', 'separator');
       taskseparator.classList.add('mdc-list-divider');
-
       const oneTask = document.createElement('li');
       oneTask.classList.add('mdc-list-item');
       oneTask.setAttribute('data_id', item.id);
-      oneTask.innerHTML = `<div class="mdc-form-field">
-      <div class="mdc-checkbox">
-        <input type="checkbox"
-              class="mdc-checkbox__native-control"
-              id="checkbox-1"/>
-        <div class="mdc-checkbox__background">
-          <svg class="mdc-checkbox__checkmark"
-              viewBox="0 0 24 24">
-            <path class="mdc-checkbox__checkmark-path"
-                  fill="none"
-                  d="M1.73,12.91 8.1,19.28 22.79,4.59"/>
-          </svg>
-          <div class="mdc-checkbox__mixedmark"></div>
-        </div>
-        <div class="mdc-checkbox__ripple"></div>
+      oneTask.innerHTML = `
+      <div class="mdc-form-field">
+        <div class="mdc-checkbox">
+          <input type="checkbox"
+                class="mdc-checkbox__native-control"
+                id="checkbox-1"/>
+          <div class="mdc-checkbox__background">
+            <svg class="mdc-checkbox__checkmark"
+                viewBox="0 0 24 24">
+              <path class="mdc-checkbox__checkmark-path"
+                    fill="none"
+                    d="M1.73,12.91 8.1,19.28 22.79,4.59"/>
+            </svg>
+            <div class="mdc-checkbox__mixedmark"></div>
+          </div>
+          <div class="mdc-checkbox__ripple"></div>
       </div>
-    </div>
-    <span class="mdc-list-item__text" ${
+      </div>
+      <span class="mdc-list-item__text" ${
   item.status ? 'style="text-decoration:line-through"' : false
 }>${item.name}</span>
-    <span class="mdc-list-item__text mdc-layout-grid--align-right">${item.time.toDate()}</span>
-      `;
+      <span class="mdc-list-item__text mdc-layout-grid--align-right">${toDate(
+    item.time,
+  )}</span>`;
       this.wrapNodeTaskList.appendChild(oneTask);
       this.wrapNodeTaskList.append(taskseparator);
     });
   }
 
-  addTask() {}
+  addTask(text) {
+    this.savedTasks.push({
+      id: (
+        Date.now().toString(36)
+        + Math.random()
+          .toString(36)
+          .substr(2, 5)
+      ).toUpperCase(),
+      name: text,
+      time: Date.now(),
+      status: false,
+    });
+    this.renderTasks();
+  }
 
-  addSelected(id) {
-    const intId = parseInt(id, 10);
-    this.select.push(intId);
+  addSelect(id) {
+    this.select.push(id);
     this.calcSelected.textContent = ` ${this.select.length}`;
   }
 
-  dellSelected(id) {
-    const intId = parseInt(id, 10);
-    const temp = this.select.filter((item) => item !== intId);
+  unSelect(id) {
+    const temp = this.select.filter((item) => item !== id);
     this.select = temp;
     this.calcSelected.textContent = ` ${this.select.length}`;
   }
@@ -189,6 +148,68 @@ class ToDo {
     this.calcSelected.textContent = ` ${this.select.length}`;
     this.renderTasks();
   }
+
+  setDone() {
+    this.select.forEach((itemID) => {
+      this.savedTasks.map((item) => {
+        if (item.id === itemID) {
+          const newState = !item.status;
+          return newState;
+        }
+        return item;
+      });
+    });
+    this.select = [];
+    this.calcSelected.textContent = ` ${this.select.length}`;
+    this.renderTasks();
+  }
 }
 
 const newTodo = new ToDo();
+const dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
+const addButton = document.querySelector('#add-task');
+
+document.addEventListener('change', (e) => {
+  if (e.target.type === 'checkbox') {
+    if (e.target.checked) {
+      newTodo.addSelect(e.target.closest('li').getAttribute('data_id'));
+    } else {
+      newTodo.unSelect(e.target.closest('li').getAttribute('data_id'));
+    }
+  }
+});
+
+document.addEventListener('keydown', (e) => {
+  if (e.target.type === 'text') {
+    const saveBtn = document.querySelector(
+      'button[data-mdc-dialog-action="save"]',
+    );
+    if (e.target.value.length >= 3) {
+      saveBtn.disabled = false;
+    } else {
+      saveBtn.disabled = true;
+    }
+  }
+});
+
+document
+  .querySelector('#delete-task')
+  .addEventListener('click', () => newTodo.deleteTask());
+
+document
+  .querySelector('#set-done')
+  .addEventListener('click', () => newTodo.setDone());
+
+addButton.addEventListener('click', () => {
+  dialog.open();
+});
+
+dialog.listen('MDCDialog:opened', () => {});
+
+dialog.listen('MDCDialog:closing', (e) => {
+  if (e.detail.action === 'save') {
+    const taskName = document.querySelector('input[name = "task-name"]');
+    newTodo.addTask(taskName.value);
+    taskName.value = '';
+  }
+});
